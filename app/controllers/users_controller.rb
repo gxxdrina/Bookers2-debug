fe312f26c81c98b198db489160bb2356c130071c
RinaOta <127483650+gxxdrina@users.noreply.github.com>
2023-04-25 13:00:39 +0000

    error 18

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+    @books = Book.all
+  end  
+
+  def edit
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to users_path
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+end
