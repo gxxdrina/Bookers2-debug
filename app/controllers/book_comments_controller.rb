ab1cb9c528a54a21e5ec43d49fa5a4cb08e24ddd
RinaOta <127483650+gxxdrina@users.noreply.github.com>
2023-05-07 07:13:23 +0000

    book_comments

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,12 +1,12 @@
 class BookCommentsController < ApplicationController
     
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = BookComment.new(book_comment_params)
     comment.user_id = current_user.id
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
+    #redirect_to book_path(book)
   end
     
   def destroy
