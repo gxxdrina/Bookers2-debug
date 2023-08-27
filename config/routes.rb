a40e135554da01963726e1caa1772a4b187814f3
RinaOta <127483650+gxxdrina@users.noreply.github.com>
2023-08-27 12:35:46 +0000

    Add chats JS

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
   
-
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]# resource(単数)だとURLにidが入らない
     resources :book_comments, only: [:create, :destroy]
@@ -20,5 +19,6 @@ Rails.application.routes.draw do
   #検索ボタンが押されたらSearchesコントローラーのsearchアクション実行
   get "search" => "searches#search"
   
+  resources :chats, only: [:show, :create]
 end
 
